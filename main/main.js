e44b8dd2feabba7385f294144cc393d3ea1ef53a
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-07 12:49:37 +0800

    Committed code changes to print the receipt

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function printReceipt(itemList) {
+    var output = '***<store earning no money>Receipt ***\n';
+    var total_amount = 0;
+    var prev_item = '';
+    var count = 0;
+
+    itemList.forEach(item => {
+        if (item.Barcode === prev_item.Barcode || count === 0) {
+            count ++;
+        } else {
+            total_amount = total_amount + (prev_item.Price*count);
+            output += 'Name: ' + prev_item.Name +', Quantity: ' + count +' ' + prev_item.Unit + 's, Unit price: ' + prev_item.Price.toFixed(2) + ' (yuan), Subtotal: ' + (prev_item.Price*count).toFixed(2) + ' (yuan)\n';
+            count = 1;
+        }
+        prev_item = item;
+    });
+    total_amount = total_amount + (prev_item.Price*count);
+    output += 'Name: ' + prev_item.Name +', Quantity: ' + count +', Unit price: ' + prev_item.Price.toFixed(2) + ' (yuan), Subtotal: ' + (prev_item.Price*count).toFixed(2) + ' (yuan)\n' +
+              '----------------------\n'+
+              'Total: ' + total_amount.toFixed(2) + ' (yuan)\n' +
+              '**********************\n';
+    return output;
 };
